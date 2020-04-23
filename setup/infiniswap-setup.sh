b9d65edf9508d01d3bca6a017d881aa2fdc9e0f4
Blake Caldwell <blake_caldwell@brown.edu>
2020-04-23 00:39:27 -0400

    infiniswap install updates

--- setup/infiniswap-setup.sh
+++ setup/infiniswap-setup.sh
@@ -42,15 +42,11 @@ prepare_infiniswap_ubuntu() {
   sudo apt-get update
   sudo apt-get remove -y kernel-mft-dkms || true
 
-  UBUNTU_RELEASE=$(cat /etc/lsb-release |grep DISTRIB_RELEASE|cut -d'=' -f2)
-  if [ -z $UBUNTU_RELEASE ]; then
-    die "Error: could not detect Ubuntu release from /etc/lsb-release"
-  fi
   if [[ "$UBUNTU_RELEASE" =~ "16.04" ]]; then
     sudo apt-get -y remove libosmcomp3 libibumad3 rdmacm-utils libmthca1 libibmad5 libipathverbs1
     sudo apt-get remove -y ibverbs-utils libibverbs-dev libibverbs1 librdmacm-dev libmlx4-dev libmlx4-1 librdmacm1
-    MLNX_OFED="MLNX_OFED_LINUX-3.4-1.0.0.0-ubuntu16.04-x86_64"
-    wget https://www.dropbox.com/s/fwxovj9jnyyrl9t/${MLNX_OFED}.tgz?dl=0 -O ${MLNX_OFED}.tgz
+    MLNX_OFED="MLNX_OFED_LINUX-4.1-1.0.2.0-ubuntu16.04-x86_64.tgz"
+    wget https://www.dropbox.com/s/qupcryelg7yyk42/${MLNX_OFED}.tgz?dl=1 -O ${MLNX_OFED}.tgz
   elif [[ "$UBUNTU_RELEASE" =~ "14.04" ]]; then
     sudo apt-get -y remove libibnetdisc5
     MLNX_OFED="MLNX_OFED_LINUX-3.4-1.0.0.0-ubuntu14.04-x86_64"
@@ -71,20 +67,46 @@ build_infiniswap() {
   git config --global user.email "you@example.com"
   git config --global user.name "Your Name"
   
-  COMMIT=master
-  git clone https://github.com/SymbioticLab/Infiniswap.git ${BUILD_DIR}
-  cd ${BUILD_DIR}
+  if [[ "$UBUNTU_RELEASE" =~ "16.04" ]]; then
+    git clone --branch without_dkms https://github.com/blakecaldwell/Infiniswap.git ${BUILD_DIR}
+  else
+    git clone https://github.com/SymbioticLab/Infiniswap.git ${BUILD_DIR}
+  fi
+  cd ${BUILD_DIR}/setup
 
-  sed -i 's/backup_disk=.*/backup_disk="/dev/sdd"/' setup/install.sh
-
-  cd setup
   export ip=$(ip a show dev ib0|grep inet|grep -v inet6|awk '{print $2}'|sed 's/\(.*\)\/.*/\1/')
   sudo ./ib_setup.sh $ip
 
   if [[ "$NAME" == "cp-1" ]]; then
-    ./install.sh daemon
+     OPTIONS="--enable-max_remote_memory=32 \
+       --enable-remote_memory_evict=8 \
+       --enable-evict_hit_limit=1 \
+       --enable-remote_memory_expand=16
+       --enable-expand_hit_limit=20 \
+       --enable-measured_free_mem_weight=0.7"
+
+    cd ${BUILD_DIR}/infiniswap_daemon
+    ./autogen.sh
+    ./configure $OPTIONS
+     make -j2
   else
-    ./install.sh bd
+    OPTIONS="--enable-max_page_num=1 \
+      --enable-bio_page_cap=32 \
+      --enable-max_remote_memory=32 \
+      --enable-stackbd_size=32 \
+      --enable-stackbd_name=stackbd \
+      --enable-backup_disk=/dev/sdb \
+      --enable-num_server_select=1"
+
+   cd ${BUILD_DIR}/infiniswap_bd
+    ./autogen.sh
+    if [[ "$UBUNTU_RELEASE" =~ "16.04" ]]; then
+      ./configure $OPTIONS --enable-lookup_bdev
+    else
+      ./configure $OPTIONS
+    fi
+    make -j2
+    sudo make install
 
     HOSTS=$(cat /etc/hosts|grep cp-|grep -v cp-1|awk '{print $4}'|sort)
     let i=0 || true
@@ -121,6 +143,10 @@ start_infiniswap() {
 
 set -e
 if [[ "$(cat /etc/lsb-release | grep DISTRIB_ID)" =~ .*Ubuntu.* ]]; then
+  export UBUNTU_RELEASE=$(cat /etc/lsb-release |grep DISTRIB_RELEASE|cut -d'=' -f2)
+  if [ -z $UBUNTU_RELEASE ]; then
+    die "Error: could not detect Ubuntu release from /etc/lsb-release"
+  fi
   prepare_infiniswap_ubuntu
   build_infiniswap
   start_infiniswap
